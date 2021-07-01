c17bba15b67225f4ff784bc80d95e9829d140fb2
Alex <31377763+AlexH893@users.noreply.github.com>
2021-07-01 14:38:12 -0500

    finishing up user-routes

--- routes/haefner-session-routes.js
+++ routes/haefner-session-routes.js
@@ -109,3 +109,88 @@ const saltRounds = 10;
         })
     }
 })
+
+
+/**
+ * Login
+ * @openapi
+ * /api/login:
+ *   POST:
+ *     tags:
+ *       - Log in
+ *     name: login
+ *     summary: Login a user
+ *     requestBody:
+ *       description: Logging in a user
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - userName
+ *               - password
+ *             properties:
+ *              userName:
+ *                 type: string
+ *              password:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: User logged in
+ *       '401':
+ *         description: Invalid username and/or password
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+ router.post('/login', async(req, res) => {
+
+    try {
+
+        User.findOne({'userName': req.params.userName}, function(err, user) {
+
+            //Create object literal named newRegisteredUser, map the RequestBody values to the object’s properties
+            if(User) {
+
+                //Compare the RequestBody password against the saved user’s password using the bcrypt.compareSync() function
+                let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+                //Checks if password is valid
+                if(passwordIsValid) {
+
+                        //Returns message for status 200
+                        console.log('Password is valid!');
+                        res.status(200).send({
+                            'message': 'User logged in'
+                        })
+                } else {
+                        //Returns message for status 401
+                        console.log('Password is invalid!');
+                        res.status(401).send({
+                            'message': 'Invalid username and/or password'
+                        })
+                }
+
+
+            } else if(!User) {
+
+                //Returns message for status 401
+                res.status(401).send({
+                    'message': `Invalid username and/or password`
+
+                })
+            }
+        })
+
+    } catch (e) {
+
+        console.log(e);
+        res.status(500).send({
+            
+            'message': `Server Exception: ${e.message}`
+        })
+    }
+})
+
+//Exporting the router using module.exports
+module.exports = router;
