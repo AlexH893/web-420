f97fc2a5f04cdd5336bb82fdf6caa505b10e72ea
Alex <31377763+AlexH893@users.noreply.github.com>
2021-07-20 21:33:28 -0500

    additions to composer routes

--- routes/haefner-composer-routes.js
+++ routes/haefner-composer-routes.js
@@ -17,7 +17,7 @@ const router = express.Router();
 /**
  * findAllComposers
  * @openapi
- * /api/composers:
+ * /api/composer
  *   get:
  *     tags:
  *       - Composers
@@ -171,5 +171,104 @@ router.get('/composer', async(req, res) => {
         })
     }
 })
+
+/**
+ * updateComposerById
+ * @openapi
+ * /api/composers/:id
+ *   put:
+ *     tags:
+ *       - Composer
+ *     name: update composer
+ *     description: API for updating an existing composer document in MongoDB
+ *     summary: Updates a document in MongoDB
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: Id is used to filter the collection by 
+ *         schema: 
+ *           type: string
+ *     requestBody:
+ *       description: Information of composer
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - firstName
+ *               - lastName
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: Array of composer documents
+ *       '401':
+ *         description: Invalid composerId
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+
+    router.put(/'/composers/:id', async (req, res) => {
+        try {
+
+            Composer.findOne({'_id': req.params.id}, function(err, composer) {
+
+                if (err) {
+
+                    console.log(err);
+                    res.status(401).send({
+
+                        'message': `Invalid composerID: ${err}`
+                    })
+
+                } else {
+
+                    console.log(compoer);
+                    //Update the returned composer object by using the set() function and mapping
+                    //the RequestBody fields to the returned objects parameters
+                    composer.set({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName
+                    })
+
+                    composer.save(function(err, updatedComposer) {
+
+                        if (err) {
+
+                            console.log(err);
+                            res.json(updatedComposer);
+
+                        } else {
+
+                            console.log(updatedComposer);
+                            res.json(updatedComposer);
+                            
+                        }
+
+                    })
+                }
+            })
+
+        } catch (e) {
+
+            console.log(e);
+            res.status(500).send({
+                'message': `Server Exception: ${e.message}`
+            })
+        }
+    })
+
+
+
+   
+
+
+
+
 
 module.exports = router 
